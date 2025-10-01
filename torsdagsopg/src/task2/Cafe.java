f292e461c9d232ef523a09d3749615d74b3aee48
Gabbyskii <gkadiga1@gmail.com>
2025-10-01 20:19:55 +0200

    task2 done

--- torsdagsopg/src/task2/Cafe.java
+++ torsdagsopg/src/task2/Cafe.java
@@ -1,5 +1,33 @@
 package task2;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Cafe {
-    
+    //2.c arraylist:
+    private ArrayList<String> coffeeMenu;
+    //constructor:
+    public Cafe() {
+        coffeeMenu = new ArrayList<>();
+    }
+   //getter:
+    public ArrayList<String> getCoffeeMenu() {
+        return coffeeMenu;
+    }
+    void loadMenuData() {
+        //2.d try-catch block to read from file
+        try {
+            //2.c file objekt:
+            File file = new File("coffees.txt");
+            Scanner scan = new Scanner(file);
+
+            //2.e loops over line of text file
+            while (scan.hasNextLine()) {
+                coffeeMenu.add(scan.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found. Check path and filename");
+        }
+    }
 }
--- torsdagsopg/src/task2/Main.java
+++ torsdagsopg/src/task2/Main.java
@@ -1,7 +1,16 @@
 package task2;
 
 public class Main {
+    //2.b main method
     public static void main(String[] args) {
+        //2.g create cafe instance and loadmenu data
+        Cafe cafe = new Cafe();
+        cafe.loadMenuData();
 
+        //2.h print all coffees from menu
+        System.out.println("\nCoffee Menu: ");
+        for (String coffee: cafe.getCoffeeMenu()) {
+            System.out.println(coffee);
+        }
     }
 }
